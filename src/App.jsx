7b38147ff610ff195934f4049625dd8c272c69d2
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2022-06-04 17:12:28 -0300

    adding sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import { Header } from "./components/Header.jsx";
 import { Post } from "./Post";
 
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar.jsx";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Gabriel Lucas"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
-      <Post author="Diego Fernandes" content="A new post" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Gabriel Lucas"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+          <Post author="Diego Fernandes" content="A new post" />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1654255455918-e3b81d74ddf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/61299646?v=4"
+        />
+
+        <strong>Gabriel Lucas</strong>
+        <span>Backend Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
